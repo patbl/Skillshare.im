48c8691982fb1e6ef0e3c2568440dd206a1ed44f
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-11 03:41:04 -0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skillshare::Application.config.secret_key_base = '95815c50d55d1513da20584f7c644db0650a548b0e121c89bf6a80531d5263b79fc7cc4c1bddb49f0d0739177e04396c465014c23508a31218ccec08defb92e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skillshare::Application.config.session_store :cookie_store, key: '_skillshare_session'
