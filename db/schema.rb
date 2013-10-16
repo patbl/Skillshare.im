e4231ae842d102dd21ea7bd412625f0da28202e0
Patrick Brinich-Langlos <pbrinichlanglois@gmail.com>
2013-10-16 01:42:24 -0800

    generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131016094053) do
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.string   "email"
+    t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
