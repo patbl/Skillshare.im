aceaa980a1c04f06bc9eccc6bc37ca2283a889b7
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-20 06:12:16 -0800

    add and configure Capybara

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ end
 
 group :test do
   gem 'simplecov', require: false
+  gem 'capybara'
 end
 
 group :production do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -42,9 +45,7 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  RSpec.configure do |config|
-    config.include FactoryGirl::Syntax::Methods
-    config.include Capybara::DSL
-  end
+  config.include FactoryGirl::Syntax::Methods
+  config.include Capybara::DSL, type: :feature
 
 end
