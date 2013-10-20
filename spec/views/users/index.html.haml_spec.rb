41becc51e51f84e6f154aea6e78f877f38c4b0c0
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-20 06:13:38 -0800

    fix view test to use render instead of Capybara methods

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe "users/index.html.haml" do
-  let!(:three_users) { create_list(:user, 3) }
+  before do
+    assign(:users, create_list(:user, 3)) 
+  end
 
   it "should display all the users" do
-    visit '/users'
-    expect(page).to have_css('#user', count: 3)
+    render
+    expect(rendered).to have_css('#user', count: 3)
   end
 end
