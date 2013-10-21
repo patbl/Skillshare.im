7534bf9de354666ec8f2e6572a26d12981acd9f3
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-21 05:20:32 -0800

    remove trailing commas from GET (whoops!)

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Skillshare::Application.routes.draw do
 
   resources :sessions, only: %i[new create destroy]
 
-  match 'auth/:provider/callback', to: 'sessions#create', via: %i[get, post]
-  match 'auth/failure', to: redirect('/'), via: %i[get, post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: %i[get, post]
+  match 'auth/:provider/callback', to: 'sessions#create', via: %i[get post]
+  match 'auth/failure', to: redirect('/'), via: %i[get post]
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: %i[get post]
 
 end
