993d0c1de339f26a8059840ef76d5fb01d595097
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-22 01:18:38 -0800

    notify user when she has signed in successfully

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe "Authentication" do
-  subject { page }
 
   describe "signin page" do
+    subject { page }
     before { visit root_url }
     it { should have_content('Facebook') }
+  end
+
+  describe "clicking on the sign-in button" do
+    before { visit root_url }
+    it "signs me in" do
+      click_link 'Sign in'
+      expect(page).to have_selector('#notice')
+    end
   end
 
 end
