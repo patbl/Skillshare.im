7552efb131bb7f58f723c78e450f4156616a08b8
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-23 10:23:21 -0800

    tidy up tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,33 +1,29 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { create(:user) }
-  let(:user_with_proposals) { create(:user_with_proposals) }
+  let(:user) { build(:user) }
+  let(:user_with_proposals) { build(:user_with_proposals) }
 
   context "validations" do
     it "is valid with a provider and a UID" do
       expect(user).to be_valid
     end
     it "is invalid without a provider" do
-      user.provider = nil
-      expect(user).to be_invalid
+      expect(build(:user, provider: nil)).to have(1).error_on(:provider)
     end
     it "is invalid without a UID" do
-      user.uid = nil
-      expect(user).to be_invalid
+      expect(build(:user, uid: nil)).to have(1).error_on(:uid)
     end
     it "is invalid with a duplicate UID" do
-      user.update_attributes uid: '123'
-      new_user = build(:user, uid: '123')
-      expect(new_user).to be_invalid
+      user.save!
+      new_user = build(:user, uid: user.uid)
+      expect(new_user).to have(1).error_on(:uid)
     end
   end
 
   describe "#full_name" do
-    let(:user) { create(:user) }
     it "returns the user's full name" do
-      user.first_name = 'Ed'
-      user.last_name = 'Lu'
+      user.assign_attributes(first_name: 'Ed', last_name: 'Lu')
       expect(user.full_name).to eq "Ed Lu"
     end
   end
