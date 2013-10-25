7a072cc651d830332815c09eee46da0416c0374c
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-25 06:43:04 -0800

    add #request? method to Proposal model

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -3,4 +3,8 @@ class Proposal < ActiveRecord::Base
   validates :title, length: { minimum: 4, maximum: 70 }
   validates :body, length: { minimum: 20, maximum: 3000 }
   validates_presence_of :location, :category, :offer
+
+  def request?
+    !offer?
+  end
 end
--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe Proposal do
+  let(:proposal) { create(:proposal) }
 
   context "validations" do
-    let(:proposal) { create(:proposal) }
     it "is valid with everything filled in" do
       expect(proposal).to be_valid
+    end
+  end
+
+  context "#request?" do
+    it "returns the opposite of what #offer? returns" do
+      proposal.offer = true
+      expect(proposal.request?).to eq(false)
+      proposal.offer = false
+      expect(proposal.request?).to eq(true)
     end
   end
 end
