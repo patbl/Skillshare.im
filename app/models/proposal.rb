25a66e4f5ccde1b260d68702a9a78c1755d40fec
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-27 23:43:56 -0800

    add offers and requests scopes

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,9 +2,12 @@ class Proposal < ActiveRecord::Base
   belongs_to :user
   validates :title, length: { minimum: 4, maximum: 70 }
   validates :description, length: { minimum: 20, maximum: 3000 }
-  validates_presence_of :location, :category, :offer, :description
+  validates_presence_of :location, :category, :description
+  scope :offers, -> { where offer: true }
+  scope :requests, -> { where offer: false }
 
   def request?
     !offer?
   end
+
 end
