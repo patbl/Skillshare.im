5a80fbd1e56dcd6f21dca66fd94697c9faa8248c
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-28 19:21:24 -0800

    allow users to edit their profiles; allow admins do delete them

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,31 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    if current_user.id.to_s == params[:id]
+      User.find(current_user).update(user_params)
+      redirect_to current_user
+    else
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    if current_user.admin?
+      User.find(params[:id]).destroy
+      redirect_to users_path, notice: "User successfully deleted."
+    else
+      redirect_to root_url 
+    end
+  end
+
+  def user_params
+    attrs = %i[email location first_name last_name]
+    params.require(:user).permit(attrs)
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,21 +1,64 @@
 require 'spec_helper'
 
 describe UsersController do
-  let(:user) { create(:user) }
-  let(:users) { create_list(:user, 2) }
+  describe "user access" do
+    let(:user) { create(:user) }
+    let(:users) { create_list(:user, 2) }
 
-  describe "GET show" do
-    it "assigns the current user to @user" do
-      get :show, id: user.id
-      expect(assigns(:user)).to_not be_nil
+    describe "GET #show" do
+      it "assigns the current user to @user" do
+        get :show, id: user.id
+        expect(assigns(:user)).to_not be_nil
+      end
+    end
+
+    describe "GET #index" do
+      it "assigns all users to @users" do
+        get :index
+        expect(assigns(:users)).to eq users
+      end
+    end
+
+    describe "GET #edit" do
+      it "allows the current user to edit her profile" do
+        session[:user_id] = user
+        get :edit, user: user
+        expect(response).to render_template :edit
+        expect(assigns(:user)).to eq user
+      end
+    end
+
+    describe "PATCH #update" do
+      it "allows the current user to update her profile" do
+        user = create(:user, last_name: "Crass")
+        session[:user_id] = user
+        patch :update, user: attributes_for(:user, last_name: "Kass"), id: user.id
+        expect(response).to redirect_to(user)
+        expect(User.find(user).last_name).to eq "Kass"
+      end
+      it "doesn't allow the current user to update another user's profile" do
+        good_user = create :user
+        bad_user = create :user
+        session[:user_id] = bad_user
+        patch :update, user: attributes_for(:user), id: good_user.id
+        expect(response).to redirect_to(root_url)
+      end
+    end
+
+    it "GET #destroy denies access" do
+      session[:user_id] = user.id
+      delete :destroy
+      expect(response).to redirect_to(root_url)
     end
   end
 
-  describe "GET index" do
-    it "assigns all users to @users" do
-      get :index
-      expect(assigns(:users)).to eq users
+  describe "admin access" do
+    it "allows administrators to delete users" do
+      unhappy_user = create :user
+      session[:user_id] = create(:admin).id
+      expect { delete :destroy, id: unhappy_user.id }.
+        to change(User, :count).by(-1)
+      expect(response).to redirect_to users_path
     end
   end
-
 end
