9b8e7dc11efc82bb22e0693e1a2d1186d792e5a3
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-29 05:04:56 -0800

    delete commented-out line (developer mode for omniauth)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :developer unless Rails.env.production?
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
            scope: 'email'
 end
