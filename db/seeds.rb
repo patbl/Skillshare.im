9678bdc6eca9f2c5f63d56e520a6a4371d054682
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-31 15:51:44 -0800

    add some seed data
    fixes #14

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+sam = User.create!({
+                     provider: 'facebook',
+                     uid: 'abc',
+                     name: 'Sam Johnson',
+                     email: 'samjohnson@gmail.com',
+                     location: 'Lichfield, England',
+                   })
+forks = Proposal.create!({
+                           title: 'forks and knives',
+                           description: "My spoons keep disappearing, so I bought a new flatware set. Now I don't know what to do with all my extra forks and knives!",
+                           location: "Lichfield, England",
+                           category: "goods",
+                           user: sam
+                         })
+
+george = User.create!({
+                        provider: "facebook",
+                        uid: "def",
+                        name: "Jorge Shrub",
+                        email: "george@hotmail.com",
+                        location: "Midland, Texas"
+                      })
+
+rubs = Proposal.create!({
+                          title: "back rubs",
+                          description: "Relax with one of my world-famous back rubs!",
+                          location: "Midland, Texas",
+                          category: "services",
+                          user: george
+                        })
+
+barack = User.create!({
+                        provider: "facebook",
+                        uid: "ghi",
+                        name: "BO",
+                        email: "potus@whitehouse.gov",
+                        location: "Washington, D.C."
+                      })
+
+drones = Proposal.create!({
+                          title: "drones",
+                          description: "Be careful with the big red button, especially if you're using them stateside.",
+                          location: "Washington, D.C.",
+                          category: "goods",
+                          user: barack
+                        })
