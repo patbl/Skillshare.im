74c92459cd8e8233de1c48727423fec187bc7445
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-10-31 15:51:44 -0800

    add some seed data
    fixes #14

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,7 +2,7 @@ class Proposal < ActiveRecord::Base
   belongs_to :user
   validates :title, length: { minimum: 4, maximum: 70 }
   validates :description, length: { minimum: 20, maximum: 3000 }
-  validates_presence_of :location, :category, :description
+  validates_presence_of :location, :category, :description, :user
 
   scope :requests, -> { where offer: false }
   scope :offers, -> { where offer: true }
