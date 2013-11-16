f6c10582e8a3a80d487af4dec2abfc581874d3ed
Ozzie Gooen <Ozzie@BowLabs.org>
2013-11-16 18:47:45 +0000

    adding icons to proposal view

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Skillshare::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  # TODO This messes up the icons, and we'll have to eventually fix that issue
   config.assets.prefix = "/dev-assets"
 
   config.middleware.use Rack::LiveReload
