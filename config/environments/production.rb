9926c515d1875918a9fce7a8187419f4950fcf87
Ozzie Gooen <Ozzie@BowLabs.org>
2013-11-19 19:09:45 +0000

    trying hint from stackoverflow here:http://stackoverflow.com/questions/10792598/heroku-ruby-on-rails-error-image-not-precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Skillshare::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: "ea-skillshare.heroku.com" }
+
+  config.assets.initialize_on_precompile = false
+
 end
