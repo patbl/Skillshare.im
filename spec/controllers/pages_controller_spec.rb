7f7e3b7acc5dae4ff761c3d7daa1feda8715a374
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-11-22 04:29:07 -0900

    add tests to ensure that static pages don't raise exceptions

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe PagesController do
+  context "#about" do
+    it "doesn't raise an exception" do
+      get :about
+    end
+  end
+
+  context "#faq" do
+    it "doesn't raise an exception" do
+      get :faq
+    end
+  end
 end
