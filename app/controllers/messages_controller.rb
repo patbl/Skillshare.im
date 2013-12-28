73902d233e0ac60cdd70be706362aedc2b4794d3
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2013-12-27 22:44:39 -0900

    fix plain-text e-mails
    also check whether order determines which method is sent in
    staging/production

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,8 @@ class MessagesController < ApplicationController
   def create
     @body = params[:message][:body]
     @offer = Offer.find(params[:offer_id])
-    UserMailer.request_mail(current_user, @body, @offer).deliver
     UserMailer.request_confirmation(current_user, @body, @offer).deliver
+    UserMailer.request_notification(current_user, @body, @offer).deliver
     redirect_back_or @offer, success: "Message sent."
   end
 end
