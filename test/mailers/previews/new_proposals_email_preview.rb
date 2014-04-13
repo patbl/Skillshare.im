1867d22e9ef261f9e1855e93ffd66375cc185df5
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2014-04-13 15:04:49 -0700

    fix e-mail style sheet

--- test/mailers/previews/new_proposals_email_preview.rb
+++
@@ -1,8 +0,0 @@
-class NewProposalsMailerPreview < ActionMailer::Preview
-  def updates_email
-    @user = FactoryGirl.create :user
-    @offers = FactoryGirl.create_list :offer, 3
-    @wanteds = FactoryGirl.create_list :wanted, 1
-    SubscriptionMailer.updates(@user, @offers, @wanteds)
-  end
-end
---
+++ test/mailers/previews/subscription_email_preview.rb
@@ -0,0 +1,8 @@
+class SubscriptionMailerPreview < ActionMailer::Preview
+  def updates_email
+    subscription = FactoryGirl.create :subscription
+    FactoryGirl.create_list :offer, 3
+    FactoryGirl.create_list :wanted, 1
+    SubscriptionMailer.updates(subscription)
+  end
+end
