b7caf63a5b7a7c12754377362acc349e41c15e50
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2014-06-01 11:36:02 -0700

    fill in last_sent column for existing users

--- db/migrate/20140601040538_add_last_sent_to_subscription.rb
+++ db/migrate/20140601040538_add_last_sent_to_subscription.rb
@@ -1,5 +1,12 @@
 class AddLastSentToSubscription < ActiveRecord::Migration
-  def change
+  def up
     add_column :subscriptions, :last_sent, :datetime
+    Subscription.all.each do |subscription|
+      subscription.update! last_sent: 10.days.ago
+    end
+  end
+
+  def down
+    remove_column :subscription, :last_sent
   end
 end
