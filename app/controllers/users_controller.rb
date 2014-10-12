9cc664fde260e383c8629813dcc3ca14ad79735d
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2014-10-12 12:01:33 -0700

    Allow users to add and edit links to EA-hub profiles.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :location, :name, :about, subscriptions_attributes: [:id, :active])
+    params.require(:user).permit(:email, :location, :name, :ea_profile, :about, subscriptions_attributes: [:id, :active])
   end
 end
