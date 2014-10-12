ec62982b43b83657438a3367a46112c745a7bf17
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2014-10-12 11:42:04 -0700

    Add EA profile link to user card.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,4 +4,16 @@ class UserDecorator < ApplicationDecorator
   def current_user?
     self == h.current_user
   end
+
+  def profile_link(attribute, logo)
+    link = user.public_send(attribute)
+    return unless link
+    h.content_tag :div, class: "user-profile-link" do
+      h.link_to link do
+        h.content_tag :div, class: "btn btn-small btn-default" do
+          h.fa_tag(logo)
+        end
+      end
+    end
+  end
 end
