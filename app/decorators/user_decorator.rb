f16d1fd26cc93211657a3fa4b6fab8680d1577d0
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2014-10-12 17:06:38 -0700

    Make EA Hub link public; make other links open in new tabs.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,11 +5,14 @@ class UserDecorator < ApplicationDecorator
     self == h.current_user
   end
 
-  def profile_link(attribute, logo)
+  def profile_link(attribute, logo, public: false, new_tab: false)
+    return unless h.signed_in? || public
     link = user.public_send(attribute)
     return unless link
     h.content_tag :div, class: "user-profile-link" do
-      h.link_to link do
+      target = "_blank" if new_tab
+      link_method = attribute == :email ? :mail_to : :link_to
+      h.public_send(link_method, link, target: target) do
         h.content_tag :div, class: "btn btn-small btn-default" do
           h.fa_tag(logo)
         end
