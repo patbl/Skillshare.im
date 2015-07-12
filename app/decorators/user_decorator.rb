4f3e1993099b30bf9e6848d13e043741d984c8d8
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2015-07-12 14:06:07 -0700

    Proposal footer: Add link to EA Hub profile.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -6,10 +6,10 @@ class UserDecorator < ApplicationDecorator
   end
 
   def facebook_link
-    return unless h.signed_in? && (link = user.facebook_url)
+    return unless h.signed_in? && user.facebook_url.present?
 
     h.content_tag :div, class: "user-profile-link" do
-      h.link_to link, target: "_blank" do
+      h.link_to user.facebook_url, target: "_blank" do
         h.content_tag :div, class: "btn btn-small btn-default" do
           h.fa_tag("facebook", tooltip: "Facebook")
         end
@@ -18,10 +18,10 @@ class UserDecorator < ApplicationDecorator
   end
 
   def email_link
-    return unless h.signed_in? && (link = user.email)
+    return unless h.signed_in? && user.email.present?
 
     h.content_tag :div, class: "user-profile-link" do
-      h.mail_to link, target: "_blank" do
+      h.mail_to user.email, target: "_blank" do
         h.content_tag :div, class: "btn btn-small btn-default" do
           h.fa_tag("envelope", tooltip: "E-mail")
         end
@@ -29,11 +29,29 @@ class UserDecorator < ApplicationDecorator
     end
   end
 
-  def ea_hub_profile_link
-    return unless (link = user.ea_profile)
+  def fancy_ea_hub_profile_link
+    return unless ea_hub_profile_link
 
     h.content_tag :div, class: "user-profile-link" do
-      h.link_to "EA Hub", link, target: "_blank", class: "ea-hub-profile-link"
+      ea_hub_profile_link(klass: "ea-hub-profile-link")
     end
+  end
+
+  def ea_hub_profile_link(klass: "")
+    return unless user.ea_profile.present?
+
+    h.link_to "EA Hub", user.ea_profile, target: "_blank", class: klass
+  end
+
+  def proposal_user_profile_link
+    if ea_hub_profile_link
+      "#{internal_profile_link} (#{ea_hub_profile_link})".html_safe
+    else
+      internal_profile_link
+    end
+  end
+
+  def internal_profile_link
+    h.link_to full_name, self
   end
 end
