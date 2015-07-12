ca0e812f2485a08778b61ddb354de70207a0a1e9
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2015-07-12 12:56:28 -0700

    EA Hub profile link: Implement Tom's more-prominent design.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,17 +5,35 @@ class UserDecorator < ApplicationDecorator
     self == h.current_user
   end
 
-  def profile_link(attribute, logo, guest_visible: false, tooltip: nil)
-    return unless h.signed_in? || guest_visible
-    link = user.public_send(attribute)
-    return unless link.present?
+  def facebook_link
+    return unless h.signed_in? && (link = user.facebook_url)
+
     h.content_tag :div, class: "user-profile-link" do
-      link_method = attribute == :email ? :mail_to : :link_to
-      h.public_send(link_method, link, target: "_blank", class: attribute) do
+      h.link_to link, target: "_blank" do
         h.content_tag :div, class: "btn btn-small btn-default" do
-          h.fa_tag(logo, tooltip: tooltip)
+          h.fa_tag("facebook", tooltip: "Facebook")
         end
       end
+    end
+  end
+
+  def email_link
+    return unless h.signed_in? && (link = user.email)
+
+    h.content_tag :div, class: "user-profile-link" do
+      h.mail_to link, target: "_blank" do
+        h.content_tag :div, class: "btn btn-small btn-default" do
+          h.fa_tag("envelope", tooltip: "E-mail")
+        end
+      end
+    end
+  end
+
+  def ea_hub_profile_link
+    return unless (link = user.ea_profile)
+
+    h.content_tag :div, class: "user-profile-link" do
+      h.link_to "EA Hub", link, target: "_blank", class: "ea-hub-profile-link"
     end
   end
 end
