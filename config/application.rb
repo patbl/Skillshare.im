4f98b9123a04dc9951476fab0c8f69d9c09b8f11
Patrick Brinich-Langlois <pat@Patricks-MBP.academia>
2015-11-28 14:01:10 -0800

    Explicitly require Tilt to suppress warning message.
    When running tests, this warning was displayed:
    
    WARN: tilt autoloading 'tilt/coffee' in a non thread-safe way; explicit
    require 'tilt/coffee' suggested.

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
+require 'tilt/coffee'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
