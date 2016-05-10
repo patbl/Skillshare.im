736d3bf8c07c0bf5895ff4802eb68b105ebee75e
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2016-05-09 21:20:33 -0700

    Switching from Mandrill to SendGrid.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "smtp.mandrillapp.com",
+  address: "smtp.sendgrid.net",
   port: 25, # ports 587 and 2525 are also supported with STARTTLS
   enable_starttls_auto: true, # detects and uses STARTTLS
-  user_name: ENV["MANDRILL_USER_NAME"],
-  password: ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+  user_name: ENV["EMAIL_SERVICE_USER_NAME"],
+  password: ENV["EMAIL_SERVICE_PASSWORD"], # SMTP password is any valid API key
   authentication: "login", # Mandrill supports 'plain' or 'login'
   domain: "skillshare.im", # your domain to identify your server when connecting
 }
