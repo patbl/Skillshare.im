e4e8563249167e91fc0758ff491655995fabbfe5
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2017-07-15 19:05:41 -0700

    Move middleware to app directory

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Skillshare
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.autoload_paths += %W( #{ config.root }/lib/NoWWW)
     config.autoload_paths += %W( #{ config.root }/lib/mail)
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
 
