e8db028dfb7e9fb03f922603bc38b3548c8751c4
Patrick Brinich-Langlois <pbrinichlanglois@gmail.com>
2020-03-19 15:50:35 -0700

    Upgrade OmniAuth and OmniAuth Facebook gems

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,20 +1,14 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  options = {
+  provider(
+    :facebook,
+    Rails.application.credentials.fetch(Rails.env.to_sym).fetch(:facebook_app_id),
+    Rails.application.credentials.fetch(Rails.env.to_sym).fetch(:facebook_app_secret),
     scope: "email,public_profile",
     image_size: :normal,
     info_fields: "first_name,last_name,location,link,email",
-    client_options: {
-      site: "https://graph.facebook.com/v2.10",
-      authorize_url: "https://www.facebook.com/v2.10/dialog/oauth",
-    },
-    token_params: { parse: :json },
-  }
-  provider :facebook,
-    Rails.application.credentials.fetch(Rails.env.to_sym).fetch(:facebook_app_id),
-    Rails.application.credentials.fetch(Rails.env.to_sym).fetch(:facebook_app_secret),
-    options
-  provider :developer, fields: %i[name], uid_field: :name if Rails.env.development?
+    # token_params: { parse: :json },
+  )
 end
 
 # in the development environment, redirect instead of raising an
